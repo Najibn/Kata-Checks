acde3e08e1b32c7bfa7cf89fabd57294080e3f3b
Najibn <kawsunagib@gmail.com>
2025-02-07 10:32:52 +0100

    in working progress

--- Dynamic-grups/grupDistribution.php
+++ Dynamic-grups/grupDistribution.php
@@ -1,25 +1,31 @@
 <?php
+// array_chunk()
+// array_rand() and shuffle
 
-function DynamicGrup($names, $numGrups)
+function DynamicGrup($names)
 {
+    $numGrups = readline("input number of groups. (dont exceed the number of names.): ");
+
     $groups = [];
     $i=0;
-    while ($i < $numGrups) {
+    while ($i < $numGrups && (!$numGrups > count($names))) {
         $groups[$i] = [];
         $i++;
     }
 
     foreach ($names as $index => $name) {
         $groupIndex = $index % $numGrups;
+        if ($groupIndex < 0 || $groupIndex >= $numGrups) {
+           // throw new Exception("Invalid group number calculated");
+        }
         $groups[$groupIndex][] = $name;
     }
-
+    
     foreach ($groups as $index => $group) {
-        echo "Group " . ($index + 1) . " => " . implode(" ", $group) .PHP_EOL;
+        echo "Group " . ($index + 1) . " => " . implode(" ,", $group) .PHP_EOL;
     }
     
 }
 
 $list = ["Pere", "Joan", "tito", "Mayte", "Julia", "alisa"];
-$numGroups = 6;
-DynamicGrup($list, $numGroups);
+DynamicGrup($list);
